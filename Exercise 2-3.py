989fbce5c5ab8e7982db443362ad5b9df2cb5a5e
Matt Dickal <matt.dickal@gmail.com>
2015-05-20 12:09:01 -0700

    Exercise 2.3

---
+++ Exercise 2-3.py
@@ -0,0 +1,8 @@
+# prompt for hours and rate to compute pay
+
+hours = raw_input('Enter Hours: ')
+hours = float(hours)
+rate = raw_input('Enter Rate: ')
+rate = float(rate)
+pay = hours * rate
+print pay
